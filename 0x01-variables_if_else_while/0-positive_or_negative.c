5d379824d43c71964090d8cfe314083565f4090e
Nezar <neln7aly110055@gmail.com>
2023-06-18 13:53:02 -0700

    uup

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - enty point
  *
@@ -8,6 +9,7 @@
  *
  * Return : 0 (success)
  */
+
 int main(void)
 {
 	int n;
@@ -15,11 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive\n" , n);
+		printf("%i is positive\n", n);
 	else if (n == 0)
-		printf("%i is zero\n" , n);
-	else 
-                printf("%i is negative\n" , n);		
-	       
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
 	return (0);
 }
