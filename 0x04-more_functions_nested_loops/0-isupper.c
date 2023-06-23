b79c399e36fe78572a0a63b975411ef7cf20e453
Nezar <neln7aly110055@gmail.com>
2023-06-22 19:29:18 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is upper
+ *
+ * Return 1 if its upperCase, 0 if not
+*/
+
+int _isuppper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
