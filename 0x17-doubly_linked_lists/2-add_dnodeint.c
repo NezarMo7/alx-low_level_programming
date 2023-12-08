afb12eb835e01f34d311b6d53ae2896ae449e6b2
NezarMo7 <neln7aly110055@gmail.com>
2023-12-08 12:33:19 -0800

    t012

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,8 +9,8 @@
  *
  * Return: address of new node or NULL
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n) {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+dlistint_t *add_dnodeint(dlistint_t **head, const int n); {	
+dlistint_t *new = malloc(sizeof(dlistint_t));
 
 	if (!head || !new) {
 		return (new ? free(new), NULL : NULL);
